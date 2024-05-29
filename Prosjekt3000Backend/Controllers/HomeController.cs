4f3bc71f263eafef1d96337e17ec56001d27ee6c
commitant <commitant@gmail.com>
2024-05-29 11:22:34 +0200

    Created bare bones backend .net core mvc project

---
+++ Prosjekt3000Backend/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Prosjekt3000Backend.Models;
+using System.Diagnostics;
+
+namespace Prosjekt3000Backend.Controllers;
+
+public class HomeController : Controller
+{
+	private readonly ILogger<HomeController> _logger;
+
+	public HomeController(ILogger<HomeController> logger)
+	{
+		_logger = logger;
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+
+	[HttpGet]
+	public IActionResult GetX()
+	{
+		return Ok("Hello World");
+	}
+}
---
+++ Prosjekt3000Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prosjekt3000Backend.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
