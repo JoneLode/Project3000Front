253613d3a7998f9b2ac552d22c0fee3c87b07561
Gard Inge Rosvold <Gard-Inge.Rosvold@ffi.no>
2024-05-29 13:26:59 +0200

    Removed redundant prosjekt-3000.sln, and added a quick suggestion (probably wrong) for database model. Is missing the connection - suggestions?

---
+++ Prosjekt3000Backend/Controllers/ProjectService.cs
@@ -0,0 +1,40 @@
+﻿using Prosjekt3000Backend.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+
+
+namespace Prosjekt3000Backend.Controllers;
+
+public class ProjectService
+{
+  private readonly IMongoCollection<ProjectModel> _projectsCollection;
+
+    public ProjectService(
+        IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            bookStoreDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            bookStoreDatabaseSettings.Value.DatabaseName);
+
+        _projectsCollection = mongoDatabase.GetCollection<ProjectModel>(
+            bookStoreDatabaseSettings.Value.BooksCollectionName);
+    }
+
+    public async Task<List<ProjectModel>> GetAsync() =>
+        await _projectsCollection.Find(_ => true).ToListAsync();
+
+    public async Task<ProjectModel?> GetAsync(string id) =>
+        await _projectsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(ProjectModel newBook) =>
+        await _projectsCollection.InsertOneAsync(newBook);
+
+    public async Task UpdateAsync(string id, ProjectModel updatedBook) =>
+        await _projectsCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+    public async Task RemoveAsync(string id) =>
+        await _projectsCollection.DeleteOneAsync(x => x.Id == id);
+}
---
+++ Prosjekt3000Backend/Models/ProjectModel.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Prosjekt3000Backend.Models;
+
+public class ProjectModel
+{
+    /*
+    •	Metadata om hvert prosjekt
+    o	Prosjektnavn
+    o	Deltakere fra SG
+    o	Oppdragsgiver
+    	Navn
+    	Avdeling
+        o	Kontaktpersoner
+        o	Beskrivelse av prosjekt
+        o	Status på prosjekt (avsluttet, under planlegging, pågående, etc)
+    o	Estimert varighet (timer?)
+    o	Forbrukt timer
+        o	Oppstartsdato
+        o	Avsluttet dato
+    o	Benyttet techstack (inkl versjoner?)
+    o	Referanse til GitLab
+    o	Gradering (hvis mulig)
+    o	Nøkkelleveranse
+    o	Læringspunkter
+    o	Retrospektive
+    */
+    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Participants { get; set; }
+
+}
